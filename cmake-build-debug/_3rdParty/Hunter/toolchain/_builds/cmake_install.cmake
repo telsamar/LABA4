3560ad1ab26fad7745815e2a9eae1eac2accba80
telsamar <Stolya2000@mail.ru>
2019-12-13 21:11:55 +0300

    work

--- cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/cmake_install.cmake
+++ cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/maus/homeworks/iu10/lab4/lab-04-boost-filesystem/cmake-build-debug/_3rdParty/Hunter/toolchain
+# Install script for directory: /cygdrive/c/Git/laba4/cmake-build-debug/_3rdParty/Hunter/toolchain
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/maus/homeworks/iu10/lab4/lab-04-boost-filesystem/cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Git/laba4/cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,31 +1,14 @@
-// Copyright 2019 <telsamar>
-#include <iostream>
+//Copyright 2019 <LinkIvan333>
+#include "FTPfiles.h"
 
-#include "BrokerResolver.h"
-
-int main(int argc, char *argv[])
-{
-    boost::filesystem::path path = "";
-    if (argc > 1) {
-        path = argv[1];
-    }
-
-    BrokerResolver resolver{};
-    resolver.resolve(path);
-
-    std::cout << "FILES: \n";
-    for (const BrokerFile &file : resolver.getFileCollection()) {
-        std::cout << file.directoriesToString() << " " <<
-                  file.filename << std::endl;
-    }
-
-    std::cout << "\nDATA: \n";
-    for (const auto &pair : resolver.getDataCollection()) {
-        std::cout << "broker:" << pair.first.broker
-                  << " account:" << pair.first.account
-                  << " files:" << pair.second.files
-                  << " lastdate:" << pair.second.lastDate
-                  << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2)
+    {
+        path path_to_ftp("C:\\Git\\laba4\\ftp");
+        iterate(path_to_ftp);
+    } else {
+        path path_to_ftp(argv[1]);
+        iterate(path_to_ftp);
     }
 
     return 0;
